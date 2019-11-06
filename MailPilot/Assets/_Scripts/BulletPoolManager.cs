b03945f4ba3f2b3bc04d95312cbe93091f17015a
Daniel Carr <daniel.carr@ontariotechu.net>
2019-11-06 16:40:17 -0500

    copy-pasting into repo

---
+++ MailPilot/Assets/_Scripts/BulletPoolManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO: Bonus - make this class a Singleton!
+
+[System.Serializable]
+public class BulletPoolManager : MonoBehaviour
+{
+    public GameObject bullet;
+
+    //TODO: create a structure to contain a collection of bullets
+
+    //array of bullets
+    Stack<GameObject> BulletClip;
+    Vector3 PoolLoc = new Vector3(-1000.0f, -1000.0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BulletClip = new Stack<GameObject>();
+        // TODO: add a series of bullets to the Bullet Pool
+        for (int i = 0; i < 25; i++)
+        {
+            GameObject newBullet = Instantiate(bullet, PoolLoc, Quaternion.identity);
+            newBullet.SetActive(false);
+            BulletClip.Push(newBullet);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //TODO: modify this function to return a bullet from the Pool
+    public GameObject GetBullet()
+    {
+        return BulletClip.Pop();
+    }
+
+    //TODO: modify this function to reset/return a bullet back to the Pool 
+    public void ResetBullet(GameObject bullet)
+    {
+        BulletClip.Push(bullet);
+        bullet.transform.position = PoolLoc;
+        bullet.SetActive(false);
+    }
+}
